23d87287f80f563febbacedfe15be7628e3524ed
rorour <ravenmarieorourke@gmail.com>
2021-02-22 20:36:11 -0700

    testing post to signup2

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ function getJSONObjectForMovieRequirement(req) {
 
 router.post('/signup', function(req, res) {
     if (!req.body.username || !req.body.password) {
-        res.json({success: false, msg: 'Please include both username and password to signup.' + req.body})
+        res.json({success: false, msg: 'Please include both username and password to signup.' + req.body.username + ' ' + req.body.password})
     } else {
         var newUser = {
             username: req.body.username,
