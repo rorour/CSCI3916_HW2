84b252f7c0563850bace18e508bef9ec25435290
rorour <ravenmarieorourke@gmail.com>
2021-02-26 17:18:02 -0700

    fixed keys

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ router.post('/signin', function (req, res) {
     } else {
         if (req.body.password === user.password) {
             var userToken = { id: user.id, username: user.username };
-            var token = jwt.sign(userToken, process.env.UNIQUE_KEY);
+            var token = jwt.sign(userToken, process.env.SECRET_KEY);
             res.json ({success: true, token: 'JWT ' + token});
         }
         else {
